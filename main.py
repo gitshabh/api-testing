095679a9c786b5f218e7b4e4acab07dfa7330073
Rishabh Srivastava <rishabhjsr2001@gmail.com>
2022-06-09 17:46:05 +0530

    added post request

--- main.py
+++ main.py
@@ -1,6 +1,15 @@
 from fastapi import FastAPI
+from model import User
+
 
 app = FastAPI()
+
+encoded_image:str
+
+@app.post("/")
+async def get_picture(user: User):
+    encoded_image = user.base64_string
+    return {"post":"successful"}
 
 @app.get("/")
 def root():
